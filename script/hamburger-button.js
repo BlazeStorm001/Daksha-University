f7e2cb4e0ba608a71f4a964cbfe91b9083086c16
BlazeStorm001 <65009329+BlazeStorm001@users.noreply.github.com>
2023-07-21 00:45:52 +0530

    added menu and made website response. Todo : add menu to all pages

---
+++ script/hamburger-button.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu-btn');
+
+function displayMenu() {
+    menu = document.querySelectorAll('nav ul li:not(:first-child)');    
+    const styles = window.getComputedStyle(menu[0]);
+    const display = styles.getPropertyValue("display");
+    console.log(`${display}`);
+    if( display == "none") {
+        menu.forEach((item) => {
+            item.style.display = "block";
+          });
+        
+    } else {
+        menu.forEach((item) => {
+            item.style.display = "none";
+        });
+    }
+}
+
+menuBtn.addEventListener('click',displayMenu);
